f8133dbb040ee45cad9814081bafb4f8eac5c0eb
alexandredgns <alexandredgns@gmail.com>
2025-08-26 15:50:16 -0300

    MVC flow - Creating new todo itens

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql://postgres:{os.environ['DB_KEY']}@localhost:5432/dummy-todoapp")
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo {self.id}, {self.description}>"
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    new_description = request.form.get('description')
+    new_todo = Todo(description=new_description)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
